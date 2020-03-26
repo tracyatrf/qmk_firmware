735f9e4afcac2451e4ee94a618832077c87725eb
Tracy <tmeade@newrelic.com>
2020-03-26 11:02:51 -0700

    new updates

--- keyboards/ergodox_ez/keymaps/tracymeade/keymap.c
+++ keyboards/ergodox_ez/keymaps/tracymeade/keymap.c
@@ -13,6 +13,7 @@ enum {
 #define P_TAB LCTL(LSFT(KC_TAB))
 #define WIN_L LCMD(KC_LEFT)
 #define WIN_R LCMD(KC_RIGHT)
+#define SPOTL LCMD(LALT(KC_SPC))
 
 #define TABNEW LCMD(KC_T)
 #define TABCLOSE LCMD(KC_W)
@@ -26,10 +27,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * |        |      |      |      |      |      | WIN_L|           |WIN_R |      |      |      |      |      |        |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * | TAB    |   Q  |   D  |   R  |   W  |   B  |      |           |      |   J  |   F  |   U  |   P  |   ;  |   BSP  |
+ * | TAB    |   Q  |   D  |   R  |   W  |   B  | ~NAV2|           |N_TAB |   J  |   F  |   U  |   P  |   ;  |   BSP  |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | LCTL   |   A  |   S  |   H  |   T  |   G  |------|           |------|   Y  |   N  |   E  |   O  |   I  |   '    |
- * |--------+------+------+------+------+------| ~NAV |           |N_TAB |------+------+------+------+------+--------|
+ * |--------+------+------+------+------+------| ~NAV |           |P_TAB |------+------+------+------+------+--------|
  * | LCMD   |   Z  |   X  |   M  |   C  |   V  |      |           |      |   K  |   L  |   ,  |   .  |   /  |  shift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   | LALT |      |      |      |      |                                       |      | Left | Down | Up   | Right|
@@ -37,16 +38,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                        ,-------------.       ,-------------.
  *                                        |zoom- |zoom+ |       |      |      |
  *                                 ,------|------|------|       |------+------+------.
- *                                 |      |      |FULL_S|       |      |      |      |
+ *                                 |      |      |FULL_S|       | SPOTL|      |      |
  *                                 | Space|Shift |------|       |------| ~SYM |Enter |
- *                                 |      |      | BKS  |       |      |      |      |
+ *                                 |      |      | BKS  |       | TAB  |      |      |
  *                                 `--------------------'       `--------------------'
  */
 
 [WORKMAN] = LAYOUT_ergodox(
   // left hand
   XXXXXXX,         XXXXXXX,     XXXXXXX,       XXXXXXX, XXXXXXX, XXXXXXX, WIN_L,
-  KC_TAB,          KC_Q,        KC_D,          KC_R,    KC_W,    KC_B,    P_TAB,
+  KC_TAB,          KC_Q,        KC_D,          KC_R,    KC_W,    KC_B,    LCMD(KC_LALT),
   KC_LCTL,         KC_A,        KC_S,          KC_H,    KC_T,    KC_G,
   KC_LCMD,         KC_Z,        KC_X,          KC_M,    KC_C,    KC_V,    MO(NAV),
   KC_LALT,         KC_HYPR,    XXXXXXX,       XXXXXXX, XXXXXXX,
@@ -57,16 +58,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   WIN_R,      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,
   N_TAB,      KC_J,    KC_F,    KC_U,    KC_P,    KC_SCLN,  KC_BSPC,
               KC_Y,    KC_N,    KC_E,    KC_O,    KC_I,     KC_QUOT,
-  XXXXXXX,    KC_K,    KC_L,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
+  P_TAB,    KC_K,    KC_L,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
   XXXXXXX,    KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,
   XXXXXXX, XXXXXXX,
-  XXXXXXX,
+  SPOTL,
   KC_TAB, MO(SYM), KC_ENT
 ),
 /* Keymap 1: Symbol Layer
  *
  * ,---------------------------------------------------.           ,--------------------------------------------------.
- * |         |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
+ * |         |      |      |   <  |   >  |      |      |           |      |      |      |      |      |      |        |
  * |---------+------+------+------+------+------+------|           |------+------+------+------+------+------+--------|
  * |    ?    |   !  |   @  |   {  |   }  |   |  |      |           |      |   +  |   7  |   8  |   9  |   *  |   \    |
  * |---------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
@@ -95,7 +96,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                         KC_TRNS,
                                    S(KC_MINUS), KC_ESC, KC_TRNS,
   // right hand
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_F10, KC_TRNS,
   TABCLOSE,KC_PLUS, KC_7,    KC_8,    KC_9,    KC_ASTR, KC_BSPC,
            KC_COLON,KC_4,    KC_5,    KC_6,    KC_EQUAL, S(KC_QUOTE),
   TABNEW,  KC_MINUS,KC_1,    KC_2,    KC_3,    KC_BSLASH, KC_TRNS,
